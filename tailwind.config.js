8f6b09b05431b2c251b204b1abcb77cde7037115
@Inforte <87519212+Inforteuz@users.noreply.github.com>
2025-10-18 18:45:23 +0500

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.vue",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
